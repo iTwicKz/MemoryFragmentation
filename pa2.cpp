5c2c1e0cf59ada80b70207c62edf85d26b1118bd
Takashi Wickes <iTwicKz@users.noreply.github.com>
2015-03-12 16:49:37 -0400

    Create pa2.cpp

---
+++ pa2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "LinkedList.cpp"
+
+using namespace std;
+
+LinkedList memorySpace;
+for(int i = 0; i < 31; i++){
+	memorySpace->insert(false, "");
+}
+
+int kBtoPages(int kB){
+	
+}
+
+void add(){
+	
+}
+int main(){
+	
+
+	
+	
+	
+	
+	
+	
+}
+
+
+void kill(){
+	cout<<"Which program would you like to kill?"<<endl;
+	cout<<"Program name - ";
+	
+}
